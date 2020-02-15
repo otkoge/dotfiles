1eb5bd53c0e2a25d0cf8aae61711e700006836bd
Gergo Kopenczei <gergo.kopenczei@skyscanner.net>
2020-02-15 09:13:38 +0100

    tmux tweaks, install go tools

--- install.sh
+++ install.sh
@@ -9,13 +9,11 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     }
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     function installer {
-        brew install
+        brew install $1
     }
 else
     echo "OS not planned for yet..."
 fi
-
-
 
 
 function install {
@@ -40,12 +38,18 @@ if [[ ! -f ~/.oh-my-zsh/oh-my-zsh.sh ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-if [[ ! -f ~/.oh-my-zsh/custom/themes/powerlevel9k ]]; then
+if [[ ! -d ~/.oh-my-zsh/custom/themes/powerlevel9k ]]; then
     echo "Installing Powerlevel9k"
     git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
     echo "Install nerd fonts: https://github.com/ryanoasis/nerd-fonts"
 fi
 
-# if [[ ! is_installed powerline ]]
-#     pip3 install --user powerline-status
-# fi
+# Go tools
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cat $DIR/tools/go_tools | grep -v '#' | grep -v -e '^$' | while read line; do
+    is_installed $(echo $line | rev | cut  -d"/" -f1 | rev)
+    if [[ $? -ne 0 ]]; then
+        go get $line
+    fi
+done 
--- setup.sh
+++ setup.sh
@@ -1,40 +1,38 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+function symlink_if_needed {
+    if [ ! -L $1 ]; then
+        ln -s $2 $1
+        echo "Symlinking $1 to $2"
+    else
+        echo "Link $1 already exists: $(ls -la $1 | rev | cut -d" " -f1 | rev)"
+    fi
+}
+
 COMP_DIR="$HOME/.completions"
-if [ ! -L $COMP_DIR ]; then
-    ln -s "$DIR/zsh/completions" "$COMP_DIR"
-fi
+symlink_if_needed $COMP_DIR "$DIR/zsh/completions" 
 
 ALIAS_FILE="$HOME/.aliases"
-if [ ! -L $ALIAS_FILE ]; then
-    ln -s "$DIR/zsh/aliases" "$ALIAS_FILE"
-fi
+symlink_if_needed $ALIAS_FILE "$DIR/zsh/aliases" 
 
 VIMRCFILE="$HOME/.vimrc"
-if [ ! -L $VIMRCFILE ]; then
-    ln -s "$DIR/vim/vimrc" "$VIMRCFILE"
-fi
+symlink_if_needed $VIMRCFILE "$DIR/vim/vimrc" 
 
 
 VIMPLUGINS="$HOME/.plugins.vim"
-if [ ! -L $VIMPLUGINS ]; then
-    ln -s "$DIR/vim/plugins.vim" "$VIMPLUGINS"
-fi
+symlink_if_needed $VIMPLUGINS "$DIR/vim/plugins.vim" 
 
 ZSHRCFILE="$HOME/.zshrc"
-if [ ! -L $ZSHRCFILE ]; then
-    ln -s "$DIR/zsh/zshrc" "$ZSHRCFILE"
-fi
+symlink_if_needed $ZSHRCFILE "$DIR/zsh/zshrc" 
 
 P9KFILE="$HOME/.powerlevel9k"
-if [ ! -L $P9KFILE ]; then
-    ln -s "$DIR/zsh/powerlevel9k" "$P9KFILE"
-fi
+symlink_if_needed $P9KFILE "$DIR/zsh/powerlevel9k" 
 
 SCRIPTFOLDER="$HOME/.scripts"
-if [ ! -L $SCRIPTFOLDER ]; then
-    ln -s "$DIR/scripts" "$SCRIPTFOLDER"
-fi
+symlink_if_needed $SCRIPTFOLDER "$DIR/scripts"
+
+TMUXCONF="$HOME/.tmux.conf"
+symlink_if_needed $TMUXCONF "$DIR/tmux/tmux.conf"
 
 for SCRIPT in "$SCRIPTFOLDER/*"; do
     chmod u+x $SCRIPT
