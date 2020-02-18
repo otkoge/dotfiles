58e667c580e6747966a093f1912627cbe561622e
otkoge <github@otkoge.hu>
2020-02-18 09:01:38 +0100

    Small install tweaks, extrac script

--- apt_install.sh
+++ apt_install.sh
@@ -1,2 +1,26 @@
 # All the needed tools
-apt install git zsh curl tmux vim python3-pip 
+
+function is_installed { 
+    if [[ $1 == "python3-pip" ]]; then
+        which pip3 &> /dev/null
+    else
+        which $1 &> /dev/null 
+    fi
+}
+
+function install {
+  is_installed $1
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    sudo apt install $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+install git
+install zsh
+install curl
+install tmux
+install vim
+install python3-pip
