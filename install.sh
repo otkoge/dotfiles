e454ad5225a0479ed4d00125e2fa0557660730de
otkoge <github@otkoge.hu>
2021-05-27 09:52:44 +0200

    add plug to nvim

--- install.sh
+++ install.sh
@@ -71,6 +71,9 @@ symlink_if_needed $NVIMCONF "$DIR/vim/init.vim"
 NVIMSLINE="$NVIM_CONF_FOLDER/statusline.vim"
 symlink_if_needed $NVIMSLINE "$DIR/vim/statusline.vim"
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 for SCRIPT in "$SCRIPTFOLDER/*"; do
     chmod u+x $SCRIPT
 done
